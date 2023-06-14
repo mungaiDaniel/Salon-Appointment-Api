3fec8f68c0a71069b69c415639b5aa28ff1993f2
mungaiDaniel <danitomonga@gmail.com>
2023-06-14 08:46:59 +0300

    added swagger

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from flask import Flask
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
-from config import DevelopmentConfig
 from flask_cors import CORS
 from app.database.database import db
 from app.auth.views import user_v1
 from app.assistances.views import assisstance_v1
 from app.service.views import service_v1
 from app.bookings.views import booking_v1
+from flask_swagger_ui import get_swaggerui_blueprint
 
 
 def create_app(config_filename):
@@ -18,6 +16,15 @@ def create_app(config_filename):
     app.config.from_object(config_filename)
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
     app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=1)
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.json'
+    SWAGGER_BLUEPRINT =get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name':'Salon Gerente'
+        }
+    )
     JWTManager(app)
     CORS(app)
     db.init_app(app)
@@ -26,6 +33,7 @@ def create_app(config_filename):
     app.register_blueprint(assisstance_v1)
     app.register_blueprint(service_v1)
     app.register_blueprint(booking_v1)
+    app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix = SWAGGER_URL)
     
 
     return app
